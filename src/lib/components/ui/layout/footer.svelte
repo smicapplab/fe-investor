5c14f2ed029be6b108f9e3c6c27079ac5f0f86b0
Steve Torrefranca <s.torrefranca@gmail.com>
2024-09-22 21:50:17 +0800

    Update footer.svelte

--- src/lib/components/ui/layout/footer.svelte
+++ src/lib/components/ui/layout/footer.svelte
@@ -1,7 +1,3 @@
-<script>
-    import { Icons } from '../icons';
-  </script>
-  
-  <footer class="flex flex-col items-center justify-center p-2 text-center text-white bg-primary">
-    <p>&copy; Koredor Capital Philippines {new Date().getFullYear()}. All Rights Reserved.</p>
-  </footer>
+<footer class="flex flex-col items-center justify-center p-2 text-center text-white bg-primary">
+	<p>&copy; Koredor Capital Philippines {new Date().getFullYear()}. All Rights Reserved.</p>
+</footer>
